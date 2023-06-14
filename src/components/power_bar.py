313a30f0149086bbcfc19b97028dcb84442518cc
ffekirnew <ffekirnew0808@gmail.com>
2023-06-14 16:49:58 +0300

    Added documentation fo rthe PowerBar class

--- src/components/power_bar.py
+++ src/components/power_bar.py
@@ -5,12 +5,31 @@ from src.configurations import white, max_power_size, screen_height, screen_widt
 
 
 class PowerBar:
+    """A class to manage the power bar in the pygame given a screen to be displayed on.
+    Attributes:
+        screen (pygame.Surface): The pygame screen to be displayed on.
+        power_size (int): The power size of the game.
+        power_bar (pygame.Rect): The pygame rectangle representing the power bar.
+    """
+
     def __init__(self, screen):
+        """Initialize the power bar.
+        Parameters:
+            screen (pygame.Surface): The pygame screen to be displayed on.
+        Return:
+            None.
+        """
         self.screen = screen
         self.power_size = 100
         self.power_bar = pygame.Rect(10, 10, (self.power_size // max_power_size) * (screen_width - 10), 10)
 
     def update(self, power_size):
+        """Update the power bar.
+        Parameters:
+            power_size (int): The power size of the game.
+        Return:
+            None.
+        """
         self.power_size = power_size
         screen_power_bar_size = min(round((self.power_size / max_power_size) * (screen_width - 10)), screen_width - 20)
         self.power_bar = pygame.Rect(10, 10, screen_power_bar_size, 10)
